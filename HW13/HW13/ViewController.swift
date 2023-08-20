550057e96d56938b6fcd8854e5086470e993bef4
yeerryy <127106050+yeerryy@users.noreply.github.com>
2023-08-20 14:18:32 +0300

    Project created

---
+++ HW13/HW13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW13
+//
+//  Created by Роман Литвинович on 20.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
